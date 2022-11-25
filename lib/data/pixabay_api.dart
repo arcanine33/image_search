9bb86171a4e68dd2f0e165fe3da4c3c96221a5c9
arcanine <leemch33@gmail.com>
2022-11-25 10:20:57 +0900

    [feat]mock client 로 응답 받을 수 있게 변경

--- lib/data/pixabay_api.dart
+++ lib/data/pixabay_api.dart
@@ -3,15 +3,16 @@ import 'dart:convert';
 import 'package:image_search/data/photo_api_repository.dart';
 import 'package:image_search/model/photo.dart';
 import 'package:http/http.dart' as http;
+
 class PixabayApi implements PhotoApiRepository{
-  final baseUrl = 'https://pixabay.com/api/';
-  final key = '31513340-a3e2d13e307b941f062f336b9';
+  static const baseUrl = 'https://pixabay.com/api/';
+  static const key = '31513340-a3e2d13e307b941f062f336b9';
 
   @override
-  Future<List<Photo>> fetch(String query) async{
-    String url = '$baseUrl?key=$key&q=$query&image_type=photo';
+  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
+    client ??= http.Client();
 
-    final response = await http.get(Uri.parse(url));
+    final response = await client.get(Uri.parse('$baseUrl?key=$key&q=$query&image_type=photo'));
 
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
     Iterable hits = jsonResponse['hits'];
