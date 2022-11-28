e22bf243204ada48c5ccf1c54b83b63e10409d6d
arcanine <leemch33@gmail.com>
2022-11-28 09:49:53 +0900

    [feat]http.client 를 외부에서 전달 받도록 수정

--- lib/data/repository/photo_api_repository_impl.dart
+++ lib/data/repository/photo_api_repository_impl.dart
@@ -1,21 +1,16 @@
-import 'dart:convert';
-
+import 'package:image_search/data/data_source/pixabay_api.dart';
 import 'package:image_search/domain/repository/photo_api_repository.dart';
-import 'package:http/http.dart' as http;
+
 import 'package:image_search/domain/model/photo.dart';
 
 class PhotoApiRepositoryImpl implements PhotoApiRepository{
-  static const baseUrl = 'https://pixabay.com/api/';
-  static const key = '31513340-a3e2d13e307b941f062f336b9';
+  PixabayApi api;
+
+  PhotoApiRepositoryImpl(this.api);
 
   @override
-  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
-    client ??= http.Client();
-
-    final response = await client.get(Uri.parse('$baseUrl?key=$key&q=$query&image_type=photo'));
-
-    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
-    Iterable hits = jsonResponse['hits'];
-    return hits.map((e) => Photo.fromJson(e)).toList();
+  Future<List<Photo>> fetch(String query) async {
+    final result = await api.fetch(query);
+    return result.map((e) => Photo.fromJson(e)).toList();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:image_search/data/data_source/pixabay_api.dart';
 import 'package:image_search/data/repository/photo_api_repository_impl.dart';
 import 'package:image_search/presentation/home/home_screen.dart';
 import 'package:provider/provider.dart';
 import 'presentation/home/home_view_model.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: ChangeNotifierProvider(
-        create: (_) => HomeViewModel(PhotoApiRepositoryImpl()),
+        create: (_) => HomeViewModel(PhotoApiRepositoryImpl(PixabayApi(http.Client()))),
         child: const HomeScreen(),
       ),
     );
