bb23d87fb6d27c6961d749a9a2da8d5edeefe82c
arcanine <leemch33@gmail.com>
2022-11-28 13:51:43 +0900

    [fix]bug 수정

--- lib/domain/usecase/get_photos_use_case.dart
+++ lib/domain/usecase/get_photos_use_case.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:image_search/data/data_source/result.dart';
 import 'package:image_search/domain/model/photo.dart';
 import 'package:image_search/domain/repository/photo_api_repository.dart';
@@ -10,7 +12,7 @@ class GetPhotosUseCase{
   Future<Result<List<Photo>>> execute(String query) async {
     final result = await repository.fetch(query);
     return result.when(success: (photos) {
-      return Result.success(photos.sublist(0, 3));
+      return Result.success(photos.sublist(0, min(3, photos.length)));
     }, error: (message) {
       return Result.error(message);
     });
